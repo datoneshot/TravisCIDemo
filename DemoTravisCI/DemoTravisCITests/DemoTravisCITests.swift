eaee7854510bcc7dec85b1da9bde30b568ef9188
datdoan <dat.one.shot.2k@gmail.com>
2020-11-06 10:48:53 +0700

    Write the first test case

--- DemoTravisCI/DemoTravisCITests/DemoTravisCITests.swift
+++ DemoTravisCI/DemoTravisCITests/DemoTravisCITests.swift
@@ -18,15 +18,7 @@ class DemoTravisCITests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssert(true == true, "Wow")
     }
 
 }
